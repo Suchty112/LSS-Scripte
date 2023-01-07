36004a0f6d26be883232bec7bdd70597b99f8e39
Tobias <Suchty112@users.noreply.github.com>
2023-01-07 20:50:27 +0100

    update versionsnumber to other forum releases

--- missionspeed.user.js
+++ missionspeed.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         MissionSpeed
-// @version      2.1.1
+// @version      2.1.3
 // @author       Allure149
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-korea\.com|missionchief-japan\.com|jocdispecerat112\.com)\/.*$/
 // @updateURL    https://github.com/types140/LSS-Scripte/raw/master/missionspeed.user.js
